77409a129dc643d7b8261557fb53347b2cb13a22
Stijn Nuiten <stijnnuiten@gmail.com>
2019-03-13 15:04:32 +0100

    fixed folder structure

--- run_pupil_preproc.py
+++ run_pupil_preproc.py
@@ -5,7 +5,7 @@ import matplotlib.pylab as pl
 from IPython import embed as shell 
 import glob
 import os 
-
+import shutil
 from hedfpy import *
 
 sn.set(style="ticks")
@@ -25,29 +25,37 @@ analysis_params = {
                 'regress_xy' : False,
                 'use_standard_blinksac_kernels' : False,
                 }
-for s in subs:
-	files = glob.glob(os.path.join(dataDir, task,str(s),'*.edf'))
-	for f in files:
-		print 'now preprocessing file ' + f
-		alias = f.split('/')[-1][:4]
 
-		# initiate HDFEyeoperator
-		h5file = os.path.join(dataDir, task,str(s) , alias + '.h5')
-		if os.path.isfile(h5file) and overwrite:
-			os.remove(h5file)
-		ho = HDFEyeOperator(os.path.expanduser(h5file))
+def preproc(subs,analysis_params):
+	for s in subs:
+		files = glob.glob(os.path.join(dataDir, task,str(s),'*.edf'))
+		for f in files:
+			print 'now preprocessing file ' + f
+			alias = f.split('/')[-1][:4]
+			preprocDir = os.path.join(dataDir, task,str(s), 'preproc/')
 
-		# extract data from EDF and run preprocessing (blink detection, filtering)
-		ho.add_edf_file(f)
-		ho.edf_message_data_to_hdf(alias = alias)
-		ho.edf_gaze_data_to_hdf(alias=alias,
-                                    sample_rate=analysis_params['sample_rate'],
-                                    pupil_lp=analysis_params['lp'],
-                                    pupil_hp=analysis_params['hp'],
-                                    normalization=analysis_params['normalization'],
-                                    regress_blinks=analysis_params['regress_blinks'],
-                                    regress_sacs=analysis_params['regress_sacs'],
-                                    use_standard_blinksac_kernels=analysis_params['use_standard_blinksac_kernels'],)
+			try:
+			    os.makedirs(os.path.join(preprocDir, 'raw/'))
+			except OSError:
+			    pass				
+			# initiate HDFEyeoperator
+			h5file = preprocDir + alias + '.h5'
+			if os.path.isfile(h5file) and overwrite:
+				os.remove(h5file)
+			ho = HDFEyeOperator(os.path.expanduser(h5file))
+
+			# extract data from EDF and run preprocessing (blink detection, filtering)
+			shutil.copy(f, os.path.join(preprocDir, 'raw', '{}.edf'.format(alias)))
+			ho.add_edf_file(os.path.join(preprocDir, 'raw', '{}.edf'.format(alias)))
+			ho.edf_message_data_to_hdf(alias = alias)
+			ho.edf_gaze_data_to_hdf(alias=alias,
+	                                    sample_rate=analysis_params['sample_rate'],
+	                                    pupil_lp=analysis_params['lp'],
+	                                    pupil_hp=analysis_params['hp'],
+	                                    normalization=analysis_params['normalization'],
+	                                    regress_blinks=analysis_params['regress_blinks'],
+	                                    regress_sacs=analysis_params['regress_sacs'],
+	                                    use_standard_blinksac_kernels=analysis_params['use_standard_blinksac_kernels'],)
 
 		# # downsample for plotting
 		# downsample_rate = 10
@@ -66,3 +74,8 @@ for s in subs:
 		# if len(eye) > 0:
 		#     eye = ['L','R'][0]
 
+def main():
+    preproc(subs=subs, analysis_params=analysis_params)
+
+if __name__ == '__main__':
+    main()
